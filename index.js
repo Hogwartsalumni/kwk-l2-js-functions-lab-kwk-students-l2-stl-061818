0f34a9532e3bc60ae06abe9e61535edaa1bb9e6d
Scarlett Patton <scarwars05@gmail.com>
2018-06-25 14:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+  return Math.abs(number - 42);
+}
+
+function distanceFromHqInFeet(number) {
+  return Math.abs((number - 42) * 264);
+}
+
+function distanceTravelledInFeet(start, finish) {
+  return Math.abs((finish - start) * 264);
+}
+
+function calculatesFarePrice(start, finish) {
+  if (distanceTravelledInFeet(start, finish) <= 400) {
+    return 'give customers a free sample';
+  } else if (((distanceTravelledInFeet(start, finish) - 400) * 0.02) > 400) {
+    return 
+  }
+}
